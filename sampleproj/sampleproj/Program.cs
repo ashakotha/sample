88e69178d5d9b31430440cfa8e8d21ede7f7ffba
ashakotha <asha.kotha@tcs.com>
2016-07-04 05:04:38 -0400

    first commit

---
+++ sampleproj/sampleproj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleproj
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+             int a,b;
+             a = 10;
+             b = 5;
+             int sum = a + b;
+             Console.WriteLine(sum);
+
+        }
+    }
+}
